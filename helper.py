2e2705618d8ee01b878cf3d799e01038767176d9
mohamedmedhat <mmr973320@gmail.com>
2024-12-25 17:25:53 +0200

    init

---
+++ helper.py
@@ -0,0 +1,16 @@
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+
+def preprocessing_step(text):
+  text = text.lower()
+  text =re.sub('[^a-zA-Z]',' ',text)
+  tokens = word_tokenize(text)
+  stop_words = set(stopwords.words('english'))
+  filtered_tokens = [word for word in tokens if word not in stop_words]
+  stemmer = PorterStemmer()
+  stemmed_tokens = [stemmer.stem(word) for word in filtered_tokens]
+  stemmed_tokens = ' '.join(stemmed_tokens)
+  return stemmed_tokens
